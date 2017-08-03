7a0876a836a3bb2222bebff3de5b6cc29dd469f5
olauby <octave.lauby@beebryte.com>
2017-08-03 11:34:41 +0800

    Update pcollection

--- pcollections/functions.py
+++ pcollections/functions.py
@@ -2,5 +2,8 @@ import numpy as np
 
 from utils.function import Function
 
-euclidean_dist = Function(lambda v1, v2: np.linalg.norm(v1 - v2))
-euclidean_dist_jac = Function(lambda v1, v2: 2 * (v1 - v2))
+euclidean_dist = Function(lambda var, const: np.linalg.norm(var - const))
+euclidean_dist_jac = Function(lambda var, const: 2 * (var - const))
+
+sigmoid = Function(lambda z: 1 / (1 + np.exp(-z)))
+sigmoid_der = Function(lambda z: sigmoid(z) * (1 - sigmoid(z)))
--- test/test_collections/test_functions.py
+++ test/test_collections/test_functions.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-from pcollections.functions import euclidean_dist
+from pcollections.functions import (
+    euclidean_dist,
+    sigmoid,
+    sigmoid_der,
+)
 
 
 def test_euclidean_dist():
     v1 = np.array([4, 5])
     v2 = np.array([1, 1])
     assert euclidean_dist(v1, v2) == 5
+
+
+def test_sigmoid():
+    inf = float('inf')
+    v = np.array([0, inf, -inf])
+    assert (sigmoid(v) == np.array([1/2, 1, 0])).all()
+    assert (sigmoid_der(v) == np.array([1/4, 0, 0])).all()
