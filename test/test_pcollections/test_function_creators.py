a63ae35b9a9e6452470a8854f54d4b7e7a22da4c
olauby <octave.lauby@beebryte.com>
2017-08-05 12:08:39 +0800

    complete fun_cre tests

--- test/test_pcollections/test_function_creators.py
+++ test/test_pcollections/test_function_creators.py
@@ -17,9 +17,19 @@ def test_StepFun():
     fun = StepFun(lambda x: x * 0.5, 2)
     assert fun(2) == 2
     assert fun(2) == 1
+    assert fun(2) == 2
+    assert fun(2) == 1
 
 
 def test_InverseDecay():
-    fun = InverseDecay(6, 2)
-    assert fun(1) == 6
-    assert fun(0) == 2
+    fun = InverseDecay(1 * 3 * 5 * 7, 2)
+    assert fun(1) == 1 * 3 * 5 * 7
+    assert fun(1000) == 1 * 5 * 7
+    assert fun(125) == 1 * 3 * 7
+    assert fun("toi") == 1 * 3 * 5
+
+
+def test_all():
+    fun = StepFun(ExponentialDecay(2), 2)
+    assert fun(0.1) == 0.1
+    assert fun(0.1) == 0.1 * np.exp(-2)
