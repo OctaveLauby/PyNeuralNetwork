049a3be9392b618bff2b1563c8b14fba0a6e4c8e
olauby <octave.lauby@beebryte.com>
2017-08-05 11:28:49 +0800

    Complete Function with +, -, /, composition

--- test/test_utils/test_function.py
+++ test/test_utils/test_function.py
@@ -1,12 +1,23 @@
 from utils.function import Function
 
 
+def compare(f1, f2):
+    for e in [-10, -5, -1.5, 0, 1.5, 5, 10]:
+        assert f1(e) == f2(e)
+    return True
+
+
 def test_Function():
     f1 = Function(lambda x: x+2)
-    f2 = Function(lambda x: x-2)
-    my_fun = f1 * f2
-    assert my_fun(2) == 0
-    assert my_fun(-2) == 0
-    assert my_fun(0) == -4
-    assert (f1 * (1/2))(2) == 2
-    assert ((1/2) * f1)(4) == 3
+    f2 = lambda x: x-2
+
+    assert compare(f1(f2), lambda x: x)
+    assert compare(f1 * f2, lambda x: (x - 2)*(x + 2))
+    assert compare(f1 + f2, lambda x: 2 * x)
+    assert compare(f1 - f2, lambda x: 4)
+    assert compare(f1 / f2, lambda x: (x + 2) / (x - 2))
+
+    assert compare(f2 * f1, f1 * f2)
+    assert compare(f2 + f1, f1 + f2)
+    assert compare(f2 - f1, lambda x: - 4)
+    assert compare(f2 / f1, lambda x: (x - 2) / (x + 2))
--- utils/function.py
+++ utils/function.py
@@ -3,18 +3,65 @@ class Function(object):
     def __init__(self, fun):
         self.fun = fun
 
-    def __mul__(self, other):
-        if isinstance(other, Function):
+    def __add__(self, other):
+        if callable(other):
             def fun(*args, **kwargs):
-                return other(*args, **kwargs) * self(*args, **kwargs)
-            return fun
+                return self(*args, **kwargs) + other(*args, **kwargs)
         else:
             def fun(*args, **kwargs):
-                return other * self.fun(*args, **kwargs)
-            return fun
+                return self.fun(*args, **kwargs) + other
+        return Function(fun)
+        return Function(fun)
+
+    def __mul__(self, other):
+        if callable(other):
+            def fun(*args, **kwargs):
+                return self(*args, **kwargs) * other(*args, **kwargs)
+        else:
+            def fun(*args, **kwargs):
+                return self.fun(*args, **kwargs) * other
+        return Function(fun)
+
+    def __neg__(self):
+        def fun(*args, **kwargs):
+            return - self(*args, **kwargs)
+        return Function(fun)
+
+    def __sub__(self, other):
+        if callable(other):
+            def fun(*args, **kwargs):
+                return self(*args, **kwargs) - other(*args, **kwargs)
+        else:
+            def fun(*args, **kwargs):
+                return self.fun(*args, **kwargs) - other
+        return Function(fun)
+
+    def __truediv__(self, other):
+        if callable(other):
+            def fun(*args, **kwargs):
+                return self(*args, **kwargs) / other(*args, **kwargs)
+        else:
+            def fun(*args, **kwargs):
+                return self.fun(*args, **kwargs) / other
+        return Function(fun)
+
+    def __radd__(self, other):
+        return self + other
 
     def __rmul__(self, other):
         return self * other
 
+    def __rsub__(self, other):
+        return (- self) + other
+
+    def __rtruediv__(self, other):
+        return Function.__truediv__(other, self)
+
     def __call__(self, *args, **kwargs):
+        if len(args) is 1 and not kwargs:
+            arg = args[0]
+            if callable(arg):
+                def fun(*args, **kwargs):
+                    return self(arg(*args, **kwargs))
+                return Function(fun)
         return self.fun(*args, **kwargs)
